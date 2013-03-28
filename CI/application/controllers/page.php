816b5c0a8ebfdbcf83d8ee383dd843d3e00f1075
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-03-29 00:23:02 +1300

    Contact Me page working

--- CI/application/controllers/page.php
+++ CI/application/controllers/page.php
@@ -162,7 +162,7 @@ class Page extends CI_Controller {
 	            
 	            // GET LIST OF WHAT IS NEEDED FOR CLASS
 	            $data['needsDetails'] = $this->Cmsmodel->getNeedsList();
-	            
+
 	            // GET PROMOTIONAL DETAILS
 	            $data['promoDetails'] = $this->Cmsmodel->getPromotion();
 	            
@@ -177,6 +177,88 @@ class Page extends CI_Controller {
 	            $this->load->view('includes/startHTML', $data);
 	            $this->load->view('classtimesView', $data);
 	            $this->load->view('includes/endHTML');
+	}
+
+	function gallery(){
+		// echo "SELECT H1
+  		// FROM tbContent
+  		// WHERE pageID =
+  		// (SELECT pageID FROM tbPages
+  		// WHERE fileName = ' ".$this->uri->segment(1)." ' )";
+		
+		// echo 'File :'. __FILE__;
+		// echo '<hr />';
+		// echo 'method: '.__FUNCTION__;
+		// echo '<hr />';
+		// echo 'uri string :'.$this->uri->uri_string();
+		// echo '<hr />';
+		// $arr = $this->uri->segment_array();
+		// echo '<pre>';
+		// 	print_r($arr);	
+		// echo '</pre>';
+
+		$data=array();
+
+	            $this->load->model('Cmsmodel');
+	            
+	            $data['menu'] = $this->Cmsmodel->getMenuParts();
+	            $data['tagline'] = $this->Cmsmodel->getTagline();
+	            
+	            // PUT THIS IN TO AVOID BROWSER CACHING IN CI
+	           	$this->output->set_header("HTTP/1.0 200 OK");
+		$this->output->set_header("HTTP/1.1 200 OK");
+		$this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+		$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
+		$this->output->set_header("Pragma: no-cache");
+
+	            $this->load->view('includes/startHTML', $data);
+	            $this->load->view('galleryView', $data);
+	            $this->load->view('includes/endHTML');
+
+	}
+
+	function contact(){
+		// echo "SELECT H1
+  		// FROM tbContent
+  		// WHERE pageID =
+  		// (SELECT pageID FROM tbPages
+  		// WHERE fileName = ' ".$this->uri->segment(1)." ' )";
+		
+		// echo 'File :'. __FILE__;
+		// echo '<hr />';
+		// echo 'method: '.__FUNCTION__;
+		// echo '<hr />';
+		// echo 'uri string :'.$this->uri->uri_string();
+		// echo '<hr />';
+		// $arr = $this->uri->segment_array();
+		// echo '<pre>';
+		// 	print_r($arr);	
+		// echo '</pre>';
+
+		$data=array();
+
+	            $this->load->model('Cmsmodel');
+	            
+	            $data['menu'] = $this->Cmsmodel->getMenuParts();
+	            $data['mainHeading'] = $this->Cmsmodel->getMainHeading();
+	            $data['tagline'] = $this->Cmsmodel->getTagline();
+
+	            // GET PROMOTIONAL DETAILS
+	            $data['promoDetails'] = $this->Cmsmodel->getPromotion();
+	            
+	            // PUT THIS IN TO AVOID BROWSER CACHING IN CI
+	           	$this->output->set_header("HTTP/1.0 200 OK");
+		$this->output->set_header("HTTP/1.1 200 OK");
+		$this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+		$this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+		$this->output->set_header("Cache-Control: post-check=0, pre-check=0");
+		$this->output->set_header("Pragma: no-cache");
+
+	            $this->load->view('includes/startHTML', $data);
+	            $this->load->view('contactView', $data);
+	            $this->load->view('includes/endHTML');
+
 	}
 
 }
