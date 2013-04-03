215fde6e22abe33ab08cc921a1a8a22654e985b7
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-04-03 14:17:46 +1300

    Edited codeigniter form validation

--- CI/application/controllers/admin.php
+++ CI/application/controllers/admin.php
@@ -29,18 +29,18 @@ class Admin extends CI_Controller {
 		
 		$this->load->helper('form');
 		
-        $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[12]|xss_clean');
-        $this->form_validation->set_rules('password', 'Password', 'trim|required|valid_password');
+                $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[5]|max_length[12]|xss_clean');
+                $this->form_validation->set_rules('password', 'Password', 'trim|required|valid_password');
 
-        if ($this->form_validation->run() == FALSE)
-        {
-            $this->load->view('loginView');
-        }
-        else
-        {
-            $this->load->view('formsuccess');
-        }
-    }
+                if ($this->form_validation->run() == FALSE)
+                {
+                    $this->load->view('loginView');
+                }
+                else
+                {
+                    $this->load->view('formsuccess');
+                }
+            }
 	
 	function update()
 	{
@@ -55,60 +55,122 @@ class Admin extends CI_Controller {
 			print_r($arr);	
 		echo '</pre>';
 	}
-	
+
+            //==============Sample add new page code==========
+
+            // make a new page
+            public function newPage()
+            {
+                // These should be autoloaded. Included here to remind you.
+                $this->load->helper(array('form', 'url'));
+                $this->load->library('form_validation');
+
+                $this->form_validation->set_rules('title', '5', 'required|min_length[5]');
+                $this->form_validation->set_rules('content', '5', 'required|min_length[5]');
+                
+                $this->form_validation->set_message('required', 'We need at least %s characters, please');
+                $this->form_validation->set_message('min_length', 'Surely you can manage %s lousy characters');
+                
+                //run the rules. If anything fails show the form
+                if ($this->form_validation->run() == FALSE){
+                    $this->load->view('includes/startHTML');
+                    $this->load->view('CMS_newPage');
+                    $this->load->view('includes/endHTML');
+                }else{
+                    // this should be in a conditional statement to handle what happens if putContent() fails
+                    $this->Cmsmodel->putContent();
+                    redirect('cms');            
+                }       
+        
+            }
+
+            // ==========end sample add new Page code===========
+
+	//==============Sample update page code==========
+
+            function updatePage()
+            {
+                $this->load->helper(array('form', 'url'));
+                $this->load->library('form_validation');
+                $this->load->model('Cmsmodel');
+        
+                $this->form_validation->set_rules('title', '5', 'required|min_length[5]');
+                $this->form_validation->set_rules('content', '5', 'required|min_length[5]');
+                
+                $this->form_validation->set_message('required', 'We need at least %s characters, please');
+                $this->form_validation->set_message('min_length', 'Surely you can manage %s lousy characters');
+                //run the rules. If anything fails show the form
+                if ($this->form_validation->run() == FALSE){
+                    $data['query_result']= $this->Cmsmodel->getContentById();
+                    $this->load->view('CMS_updatePage',$data);
+                    
+                }else{
+                    if($this->Cmsmodel->updateContent() ){
+                        redirect('cms');
+                    }else{
+                        redirect('cms/admin');
+                    }
+                    
+                } 
+            }
+
+            // ===========end sample update Page code============
+
+
+
 	function homeUpdate()
 	{
-        $data=array();
+                $data=array();
 
-        $this->load->model('Cmsmodel');
-        
-        $data['menu'] = $this->Cmsmodel->getMenuParts();
-        $data['mainHeading'] = $this->Cmsmodel->getMainHeading();
-        $data['tagline'] = $this->Cmsmodel->getTagline();
-        
-        // PUT THIS IN TO AVOID BROWSER CACHING IN CI
-        $this->output->set_header("HTTP/1.0 200 OK");
-        $this->output->set_header("HTTP/1.1 200 OK");
-        $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-        $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
-        $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
-        $this->output->set_header("Pragma: no-cache");
+                $this->load->model('Cmsmodel');
+                
+                $data['menu'] = $this->Cmsmodel->getMenuParts();
+                $data['mainHeading'] = $this->Cmsmodel->getMainHeading();
+                $data['tagline'] = $this->Cmsmodel->getTagline();
+                
+                // PUT THIS IN TO AVOID BROWSER CACHING IN CI
+                $this->output->set_header("HTTP/1.0 200 OK");
+                $this->output->set_header("HTTP/1.1 200 OK");
+                $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+                $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+                $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
+                $this->output->set_header("Pragma: no-cache");
 
-        $this->load->view('includes/startHTML', $data);
-        $this->load->view('aboutUpdate', $data);
-        $this->load->view('includes/endHTML');
-    }
+                $this->load->view('includes/startHTML', $data);
+                $this->load->view('homeUpdate', $data);
+                $this->load->view('includes/endHTML');
+            }
 	
     
-    function aboutUpdate()
-    {
-        $data=array();
+            function aboutUpdate()
+            {
+                $data=array();
 
-        $this->load->model('Cmsmodel');
-        
-        $data['menu'] = $this->Cmsmodel->getMenuParts();
-        $data['mainHeading'] = $this->Cmsmodel->getMainHeading();
-        $data['tagline'] = $this->Cmsmodel->getTagline();
+                $this->load->model('Cmsmodel');
+                
+                $data['menu'] = $this->Cmsmodel->getMenuParts();
+                $data['mainHeading'] = $this->Cmsmodel->getMainHeading();
+                $data['tagline'] = $this->Cmsmodel->getTagline();
 
-        // GET LIST OF TESTIMONIALS FOR ABOUT PAGE 
-        $data['testimonialDetails'] = $this->Cmsmodel->getTestimonials();
-        $data['name'] = $this->Cmsmodel->getName();
-        
-        // GET PROMOTIONAL DETAILS
-        $data['promoDetails'] = $this->Cmsmodel->getPromotion();
-        
-        // PUT THIS IN TO AVOID BROWSER CACHING IN CI
-        $this->output->set_header("HTTP/1.0 200 OK");
-        $this->output->set_header("HTTP/1.1 200 OK");
-        $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-        $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
-        $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
-        $this->output->set_header("Pragma: no-cache");
+                // GET LIST OF TESTIMONIALS FOR ABOUT PAGE 
+                $data['testimonialDetails'] = $this->Cmsmodel->getTestimonials();
+                $data['name'] = $this->Cmsmodel->getName();
+                
+                // GET PROMOTIONAL DETAILS
+                $data['promoDetails'] = $this->Cmsmodel->getPromotion();
+                
+                // PUT THIS IN TO AVOID BROWSER CACHING IN CI
+                $this->output->set_header("HTTP/1.0 200 OK");
+                $this->output->set_header("HTTP/1.1 200 OK");
+                $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+                $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
+                $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
+                $this->output->set_header("Pragma: no-cache");
 
-        $this->load->view('includes/startHTML', $data);
-        $this->load->view('aboutUpdate', $data);
-        $this->load->view('includes/endHTML');
-    }
+                $this->load->view('includes/startHTML', $data);
+                $this->load->view('aboutUpdate', $data);
+                $this->load->view('includes/endHTML');
+            }
     
     function classtimesUpdate(){
         
