c495ca334698bc15ca67ed6fbc4108c0909f92b8
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-04-03 16:11:28 +1300

    Removed HTML header

--- CI/application/views/classesUpdate.php
+++ CI/application/views/classesUpdate.php
@@ -1,39 +1,4 @@
-<!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Zumba with Christina | Classes</title>
-  <meta name="viewport" content="width=device-width,initial-scale=1">
-  <link rel="stylesheet" href="stylesheets/_main.css">
- <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
-  <script src="javascripts/modernizr-2.6.2.min.js"></script> <!-- * -->
-</head>
-<body>
-    
-  
-  <div id="wrapper"> <!-- wrapper begins-->
-      
-    <header id="headerContainer"> <!--logo and menu container begins-->
-      
-      <div id="logo"><!-- logo begins -->
-        <a href="home.html">logo</a>
-      </div><!--logo ends-->     
-      
-      <nav id="navBar"> <!--navigation bar begins-->
-        <ul id="menu">
-          <li class="home"><a href="indexUpdate.html">HOME</a></li>
-          <li class="about"><a href="aboutUpdate.html">ABOUT ME</a></li>
-          <li class="classtimes"><a href="classesUpdate.html">CLASSES</a></li>
-          <li class="gallery"><a href="gallery.html">VIDEO</a></li>
-          <li class="contact"><a href="contact.html">Contact Me</a></li>
-        </ul>
-      </nav> <!--navigation bar ends--> 
-    </header><!--logo and menu container ends-->
-    
+
     <section id="mainContainer" class="cf"><!--class schedule container begins-->
     
       <form>
