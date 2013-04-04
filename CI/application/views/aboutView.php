95b0a86f60e3724f6f32839e14a0d6bdc8abf802
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-04-04 16:24:38 +1300

    Tidied and indented

--- CI/application/views/aboutView.php
+++ CI/application/views/aboutView.php
@@ -1,92 +1,70 @@
-   
-    <section id="mainContainer" class="cf"><!--main container begins-->  
+<section id="mainContainer" class="cf"><!--main container begins-->  
     
-      <h1 class="aboutHeading"><!-- ABOUT ME -->
-            <?php  
-                  $aPageParts = $pageParts->row_array();
-                  echo nl2br($aPageParts['H1']);
-            ?>
-      </h1>
+  <h1 class="aboutHeading"><!-- ABOUT ME -->
+    <?php  
+          $aPageParts = $pageParts->row_array();
+          echo nl2br($aPageParts['H1']);
+    ?>
+  </h1>
     
-      <section id="about"> <!--about me content begins-->    
-        
-        <article class="info"><!--content paragraphs begin-->
-            <p>
-            <?php 
-                  // $oContent = $content->row_array();
-                  //echo nl2br($oContent['contentDetails']);
-            echo nl2br($aPageParts['contentDetails']);
-            ?>
-            </p>
-        </article><!--content paragraphs end-->
-        
-      </section> <!--about me content ends--> 
-        
+  <section id="about"> <!--about me content begins-->    
+    <article class="info"><!--content paragraphs begin-->
+      <p>
+      <?php 
+        echo nl2br($aPageParts['contentDetails']);
+      ?>
+      </p>
+    </article><!--content paragraphs end-->
+  </section> <!--about me content ends--> 
          
-      <section id="rightContent" class="cf"> <!--right hand side content begins-->
-        
-        <section id="selfContainer">
-          <article class="selfPhoto">
-            <!--<img src="assets/images/zumbaLogo.jpg"/>-->
-          </article>
-        </section>
+  <section id="rightContent" class="cf"> <!--right hand side content begins-->
+    <section id="selfContainer">
+      <article class="selfPhoto">
+        <!--<img src="assets/images/zumbaLogo.jpg"/>-->
+      </article>
+    </section>
+  </section> <!--right hand side content ends-->
 
-      </section> <!--right hand side content ends-->
+  <section id="testimonials"><!--testimonials begin-->
+    <h3>
+    <?php 
+      echo nl2br($aPageParts['H3']);
+    ?>
+    </h3>
 
-       <section id="testimonials"><!--testimonials begin-->
-        <h3>
-        <?php 
-              // $oSubHeading = $subHeading->row_array();
-              // echo nl2br($oSubHeading['H3']);
-
-            echo nl2br($aPageParts['H3']);
-        ?>
-        </h3>
-
-        <ul>
-
-          <?php
-          
-          $aTestimonialDetails= $testimonialDetails->result_array();
-          // $aName= $name->result_array();
-
-        foreach($aTestimonialDetails as $key=>$aTestimonials){
-         // echo $aValue['testimonialDetails'];
-
-          echo '<li> '.  $aTestimonials['testimonialDetails']  . '<span class="reference"> ' .  $aTestimonials['name'] . '</span></li>';
-        }
-
-
-          
-          // echo "<pre>";
-          // print_r($aTestimonialDetails);
-          // echo "</pre>";
-          ?>
-          
-         
-        </ul>
-      </section><!--testimonials ends-->
+    <ul>
+    <?php
+      $aTestimonialDetails= $testimonialDetails->result_array();
       
-      <section><!--tagline begins-->
-        <h2 id="aboutTag">
-              <?php 
-                    $oTagline = $tagline->row();
-                    echo nl2br($oTagline->tagline);
-              ?>
-        </h2>
-      </section><!--tagline ends-->
+      foreach($aTestimonialDetails as $key=>$aTestimonials){
+        echo '<li> '.  $aTestimonials['testimonialDetails']  . '<span class="reference"> ' .  $aTestimonials['name'] . '</span></li>';
+      }
+      // echo "<pre>";
+      // print_r($aTestimonialDetails);
+      // echo "</pre>";
+      ?>
+      </ul>
+    </section><!--testimonials ends-->
       
-    </section><!--main container ends-->
+    <section><!--tagline begins-->
+      <h2 id="aboutTag">
+      <?php 
+            $oTagline = $tagline->row();
+            echo nl2br($oTagline->tagline);
+      ?>
+      </h2>
+    </section><!--tagline ends-->
+      
+  </section><!--main container ends-->
     
-    <section id="promo" class="promoAbout"><!--promotional tag begins-->
-           <p><!--Mention this website and receive <span class="prmsgAbout">50%</span> off all classes in your first week OR bring a friend for <span class="prmsgAbout">FREE!</span>--> 
-            <?php 
-              $oPromoDetails = $promoDetails->row();
-              echo nl2br($oPromoDetails->promoDetails);
-        ?></p>
-    </section><!--promotional tag ends-->
+  <section id="promo" class="promoAbout"><!--promotional tag begins-->
+    <p><!--Mention this website and receive <span class="prmsgAbout">50%</span> off all classes in your first week OR bring a friend for <span class="prmsgAbout">FREE!</span>--> 
+      <?php 
+        $oPromoDetails = $promoDetails->row();
+        echo nl2br($oPromoDetails->promoDetails);
+      ?>
+    </p>
+  </section><!--promotional tag ends-->
     
-  </div> <!--wrapper ends-->
-    
-    
+</div> <!--wrapper ends-->
     
