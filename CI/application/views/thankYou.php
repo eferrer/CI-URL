454e8d71a62152a887e4d58bcd6b8ad02b5f7047
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-04-11 09:29:34 +1200

    Thank you message for enquiry

---
+++ CI/application/views/thankYou.php
@@ -0,0 +1,9 @@
+<section class="mainContainer" class="cf"><!--main container begins-->
+    <h3 class="thankYou">Thank you for your enquiry</h3>
+    <section class="contentAdmin">
+
+<p class="thankYou">I will be in touch with you shortly. Christina</p>
+
+</section>
+</section>
+</div>
