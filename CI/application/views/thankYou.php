bbdee9581f6260d2164fb403ce3c63be4c37b496
Eileen Ferrer <eileenferrer.nz@gmail.com>
2013-04-11 10:25:59 +1200

    Styled thank you for enquiry page

--- CI/application/views/thankYou.php
+++ CI/application/views/thankYou.php
@@ -1,8 +1,8 @@
-<section class="mainContainer" class="cf"><!--main container begins-->
-    <h3 class="thankYou">Thank you for your enquiry</h3>
+<section class="mainContainer" class=""><!--main container begins-->
+    <h3 class="thankYouHeading">Thank you for your enquiry</h3>
     <section class="contentAdmin">
 
-<p class="thankYou">I will be in touch with you shortly. Christina</p>
+<p class="thankYouMessage">I will be in touch with you shortly.<br/>Christina</p>
 
 </section>
 </section>
